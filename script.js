d529f548d71142ebb20bb825e373c39aa318f9c2
siddharthkumar021 <siddharthakumar4298@gmail.com>
2024-01-21 21:38:05 +0530

    make newjavascript project

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // get randomColor form 0 to hex through Math.random
 
 
-const randomColor = function(){
-    const hex = '0123456789ABCDEF';
-    let color ="#"
-    for(let i = 0; i < 6; i++){
-        color += hex[Math.floor(Math.random() *16)]
-    }
-    return color
-};
+//const randomColor = function(){
+   // const hex = '0123456789ABCDEF';
+   // let color ="#"
+    //for(let i = 0; i < 6; i++){
+        // color += hex[Math.floor(Math.random() *16)]
+    //}
+    // return color
+//};
 
 
-let intervalId;
-const startChangingColor = function(){
-    if(!intervalId){
-        intervalId = setInterval(changeBgColor, 1000)
-    }
+// let intervalId;
+// const startChangingColor = function(){
+    //if(!intervalId){
+     //   intervalId = setInterval(changeBgColor, 1000)
+    //}
     
-    function changeBgColor(){
-        document.body.style.backgroundColor = randomColor();
-    }
-};
+   // function changeBgColor(){
+       // document.body.style.backgroundColor = randomColor();
+    //}
+// };
 
-const stopChangingColor = function(){
-    clearInterval(intervalId);
-    intervalId = null; // if your varible are reffer to not use than put there value is null for memory can free
-}
+//const stopChangingColor = function(){
+    // clearInterval(intervalId);
+    // intervalId = null; // if your varible are reffer to not use than put there value is null for memory can free
+//}
 
 
-// document.querSelector are use to allow to select and mainpulate HTML it is part of dom(document object model)
-document.querySelector("#start").addEventListener('click', startChangingColor)
-document.querySelector("#stop").addEventListener('click', stopChangingColor)
+          /// document.querSelector are use to allow to select and mainpulate HTML it is part of dom(document object model)
+// document.querySelector("#start").addEventListener('click', startChangingColor)
+// document.querySelector("#stop").addEventListener('click', stopChangingColor)
